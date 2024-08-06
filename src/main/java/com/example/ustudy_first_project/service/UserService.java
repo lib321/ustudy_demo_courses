64538b1f17020a1e5515610bd4885749d067640d
Alibek Abilkassymov <al16ek199@gmail.com>
2024-08-06 23:23:51 +0500

    added user repository and user service"

--- src/main/java/com/example/ustudy_first_project/service/UserService.java
+++ src/main/java/com/example/ustudy_first_project/service/UserService.java
@@ -1,8 +1,45 @@
 package com.example.ustudy_first_project.service;
 
 
+import com.example.ustudy_first_project.entity.User;
+import com.example.ustudy_first_project.repository.UserRepository;
+import com.example.ustudy_first_project.service.register.RegistrationRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(RegistrationRequest registrationRequest) {
+        if (!registrationRequest.getPassword().equals(registrationRequest.getRepeatPassword())) {
+            throw new IllegalArgumentException("Пароли не совпадают");
+        }
+
+        if (userRepository.findByPhone(registrationRequest.getPhone()).isPresent()) {
+            throw new IllegalArgumentException("Телефон уже зарегистрирован");
+        }
+
+        User user = new User();
+        user.setName(registrationRequest.getName());
+        user.setPhone(registrationRequest.getPhone());
+        user.setEmail(registrationRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+        user.setRole(registrationRequest.getRole());
+
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByPhone(String phone) {
+        return userRepository.findByPhone(phone);
+    }
 }
