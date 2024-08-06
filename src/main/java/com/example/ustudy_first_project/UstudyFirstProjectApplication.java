399095bfe9416806f9f77fbdef980a76b4d435b8
Alibek Abilkassymov <al16ek199@gmail.com>
2024-08-06 22:37:33 +0500

    empty project

---
+++ src/main/java/com/example/ustudy_first_project/UstudyFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ustudy_first_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstudyFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UstudyFirstProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ustudy_first_project/UstudyFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ustudy_first_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UstudyFirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
